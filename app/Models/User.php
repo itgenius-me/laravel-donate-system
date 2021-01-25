2fb35c1057ca25cff34e7a1623ea0d63ee2bf799
Devdreamsolution <q3construction1@gmail.com>
2021-01-26 09:48:15 +1000

    remove require email verification function

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,8 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 use App\Helpers\HasGuidTrait;
 
-class User extends Authenticatable implements MustVerifyEmail
+// class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens, HasGuidTrait;
 
