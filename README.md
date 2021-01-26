a3ee7a11f7c9f6f5873a0d6f0f378b965fe6c78f
Devdreamsolution <q3construction1@gmail.com>
2021-01-26 21:19:16 +1000

    add cellphone field

--- README.md
+++ README.md
@@ -28,3 +28,6 @@ php artisan serve
 
 http://localhost:8000
 ```
+
+## IP Geolocation API
+IP Geolocation API is a free real-time IP to [Geolocation](https://ipgeolocationapi.com/) JSON API with detailed countries data integration.
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,6 +40,20 @@
                         </div>
 
                         <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Cellphone') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="cellphone" type="email" class="form-control @error('cellphone') is-invalid @enderror" name="cellphone" value="{{ old('cellphone') }}" required autocomplete="cellphone">
+
+                                @error('cellphone')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
