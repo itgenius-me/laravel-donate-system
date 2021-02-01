7121523b7176e58485e4770d825c79670d4c410b
Devdreamsolution <q3construction1@gmail.com>
2021-02-02 00:56:26 +1000

    Seeder permission and role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\Schema;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Artisan::call('passport:install');
+        Schema::disableForeignKeyConstraints();
+        Permission::truncate();
+        Role::truncate();
+        DB::table('model_has_permissions')->truncate();
+        DB::table('model_has_roles')->truncate();
+        DB::table('role_has_permissions')->truncate();
+        $this->call(PermissionSeeder::class);
         $this->call(UserSeeder::class);
+        Schema::enableForeignKeyConstraints();
     }
 }
