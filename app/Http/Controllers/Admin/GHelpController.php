c74048d9b08b9ad371702819fcc681fc8dfa974f
kcaribou <kcaribou0428@gmail.com>
2021-02-09 23:50:31 +0800

    create get-help crud

---
+++ app/Http/Controllers/Admin/GHelpController.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\GHelp;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Yajra\DataTables\DataTables;
+
+class GHelpController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        $totalData = GHelp::query()->count();
+
+        $totalFiltered = $totalData;
+
+        if ($request->ajax()) {
+            $limit = $request->input('length');
+            $start = $request->input('start');
+
+            if(empty($request->input('search.value')))
+            {
+                $g_helps = GHelp::query()->offset($start)
+                    ->limit($limit)
+                    ->get();
+            } else {
+                $search = $request->input('search.value');
+                $g_helps =  GHelp::query()->where('id','LIKE',"%{$search}%")
+                    ->orWhere('email', 'LIKE',"%{$search}%")
+                    ->offset($start)
+                    ->limit($limit)
+                    ->get();
+                $totalFiltered = GHelp::query()->where('id','LIKE',"%{$search}%")
+                    ->orWhere('email', 'LIKE',"%{$search}%")
+                    ->count();
+            }
+            $result = Datatables::of($g_helps)
+                ->addIndexColumn()
+                ->addColumn('leader_email', function ($row) {
+                    $user = User::query()->where('email', $row->email)->get();
+                    if ($user) $user_id = $user->first()->id;
+                    else return "";
+                    return User::getLeaderEmail($user_id);
+                })
+                ->addColumn('country', function ($row) {
+                    $user = User::query()->where('email', $row->email)->get();
+                    if ($user) $user = $user->first();
+                    else return "";
+                    $jsonCountries = file_get_contents(base_path('public/assets/countries.json'));
+                    $arrayCountries = json_decode($jsonCountries, true);
+                    $arrayKey = array_search($user->cellphone_code, array_column($arrayCountries, 'ISD'));
+                    return $arrayCountries[$arrayKey]['NAME'];
+                })
+                ->addColumn('date_of_get_help', function ($row) {
+                    return Carbon::parse($row->created_at)->toDateTimeString();
+                })
+                ->addColumn('type', function ($row) {
+                    if ($row->type == 1)
+                        return '<span class="label label-purple">'. __('global.OrderManage.GetHelp.Local') .'</span>';
+                    else
+                        return '<span class="label label-info">'. __('global.OrderManage.GetHelp.Cripto') .'</span>';
+                })
+                ->addColumn('status', function ($row) {
+                    if ($row->status == 1)
+                        return '<span class="label label-success">'. __('global.OrderManage.GetHelp.Confirmed') .'</span>';
+                    else
+                        return '<span class="label label-warning">'. __('global.OrderManage.GetHelp.Unconfirmed') .'</span>';
+                })
+                ->addColumn('action', function ($row) {
+//                    if (request()->user()->id === $row->id)
+//                        $disabled = 'disabled';
+//                    else
+//                        $disabled = '';
+
+                    $btn = '<a href="'. route('admin.get-help.edit', $row->id) .'" data-id="'.$row->id.'" class="edit btn btn-primary btn-sm mb-1"><i class="far fa-edit"></i></a>';
+                    $btn .= ' <button onclick="deleteGetHelp('. "'$row->id'" .')" data-id="'.$row->id.'" class="btn btn-danger btn-sm mb-1"><i class="far fa-trash-alt"></i></button>';
+                    $btn .= '<form id="deleteForm'. $row->id .'" action="'. route('admin.get-help.destroy', $row->id) .'" method="POST" style="display: none">
+                    <input type="hidden" name="_token" value="'. csrf_token() .'">
+                    <input type="hidden" name="_method" value="DELETE">
+                    @method("DELETE")
+                    </form>';
+
+                    return $btn;
+                })
+                ->skipPaging()
+                ->setTotalRecords($totalData)
+                ->setFilteredRecords($totalFiltered)
+                ->rawColumns(['status', 'action', 'type'])
+                ->make(true);
+            return $result;
+        }
+
+        return view('admin.get-help.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     */
+    public function create()
+    {
+        $emails = User::all()->pluck('email');
+        return view('admin.get-help.create', compact('emails'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => ['exists:users,email', 'required'],
+            'amount' => ['required', 'regex:/^[0-9]+(\.[0-9][0-9]?)?$/']
+        ], [
+            'amount.regex' => 'The :attribute should be double',
+        ]);
+        $validated['type'] = $request->type;
+        GHelp::create($validated);
+
+        return redirect('/admin/get-help');
+    }
+
+    /**
+     * Show the form for editing a existing resource.
+     *
+     */
+    public function edit($id)
+    {
+        $emails = User::all()->pluck('email');
+        $gHelp = GHelp::query()->find($id);
+        return view('admin.get-help.edit', compact('emails', 'gHelp'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     */
+    public function update(Request $request, $id)
+    {
+        $gHelp = GHelp::query()->find($id);
+        $validated = $request->validate([
+            'email' => ['exists:users,email', 'required'],
+            'amount' => ['required', 'regex:/^[0-9]+(\.[0-9][0-9]?)?$/']
+        ], [
+            'amount.regex' => 'The :attribute should be double',
+        ]);
+        $validated['type'] = $request->type;
+        $gHelp->update($validated);
+
+        return redirect('/admin/get-help');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     */
+    public function destroy($id)
+    {
+        $gHelp = GHelp::query()->find($id);
+        $gHelp->delete();
+
+        return redirect('/admin/get-help');
+    }
+
+}
---
+++ resources/views/admin/get-help/index.blade.php
@@ -0,0 +1,145 @@
+@extends('layouts.app')
+@section('content')
+<div class="row page-titles">
+    <div class="col-md-5 align-self-center">
+        <h4 class="text-themecolor">{{ trans('global.OrderManage.GetHelp.title') }}</h4>
+    </div>
+    <div class="col-md-7 align-self-center text-right">
+        <div class="d-flex justify-content-end align-items-center">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item">{{ trans('global.OrderManage.title') }}</li>
+                <li class="breadcrumb-item active">{{ trans('global.OrderManage.GetHelp.title') }}</li>
+            </ol>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    @if ($message = Session::get('success'))
+    <div class="alert alert-success">
+        <i class="ti-user"></i> {{ $message }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+    </div>
+    @endif
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <a href="{{ url('admin/get-help/create') }}" class="btn waves-effect waves-light btn-primary"><i class="ti-plus text"></i> {{ trans('global.OrderManage.GetHelp.Create') }}</a>
+                    <div class="table-responsive">
+                        <table id="dataTable" class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>{{ trans('global.Email') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Email-Manager') }}</th>
+                                    <th>{{ trans('global.Country') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Date') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Amount') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Confirmed-Amount') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Type') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Status') }}</th>
+                                    <th width="100px">{{ trans('global.Action') }}</th>
+                                </tr>
+                            </thead>
+                            <tfoot>
+                                <tr>
+                                    <th>#</th>
+                                    <th>{{ trans('global.Email') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Email-Manager') }}</th>
+                                    <th>{{ trans('global.Country') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Date') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Amount') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Confirmed-Amount') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Type') }}</th>
+                                    <th>{{ trans('global.OrderManage.GetHelp.Status') }}</th>
+                                    <th>{{ trans('global.Action') }}</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('css')
+<link href="{{ asset('assets/node_modules/datatables/media/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/node_modules/sweetalert/sweetalert.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset('assets/node_modules/toast-master/css/jquery.toast.css') }}" rel="stylesheet">
+<style>
+    .jq-icon-info {
+        background-color: #03a9f3;
+        color: #fff;
+    }
+
+    .jq-icon-success {
+        background-color: #00c292;
+        color: #fff;
+    }
+
+    .jq-icon-error {
+        background-color: #e46a76;
+        color: #fff;
+    }
+
+    .jq-icon-warning {
+        background-color: #fec107;
+        color: #fff;
+    }
+
+    .alert-rounded {
+        border-radius: 60px;
+    }
+</style>
+@endpush
+
+@push('js')
+<script src="{{ asset('assets/node_modules/datatables/datatables.min.js') }}"></script>
+<script src="{{ asset('assets/node_modules/sweetalert/sweetalert.min.js') }}"></script>
+<script src="{{ asset('assets/node_modules/toast-master/js/jquery.toast.js') }}"></script>
+<script>
+    var dataTable = $('#dataTable').DataTable({
+        'processing': true,
+        'serverSide': true,
+        "ordering": false,
+        'ajax': {
+            'url': "{{ url('admin/get-help') }}",
+            'type': 'GET'
+        },
+        'columns': [
+            {'data': 'id'},
+            {'data': 'email'},
+            {'data': 'leader_email'},
+            {'data': 'country'},
+            {'data': 'date_of_get_help'},
+            {'data': 'amount'},
+            {'data': 'confirmed_amount'},
+            {'data': 'type'},
+            {'data': 'status'},
+            {'data': 'action'},
+        ],
+    });
+
+    function deleteGetHelp(id) {
+        Swal.fire({
+            title: "{{ trans('global.Swal.DelConfirm.title') }}",
+            text: "{{ trans('global.Swal.DelConfirm.text') }}",
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: "{{ trans('global.Swal.DelConfirm.yes') }}",
+            confirmButtonClass: 'btn btn-primary',
+            cancelButtonClass: 'btn btn-danger ml-1',
+            buttonsStyling: false,
+        }).then(function (result) {
+            if (result.value) {
+                $('#deleteForm'+id).submit();
+            }
+        })
+    }
+
+</script>
+@endpush
