ada1ed937581e7e8ed2865a4dcf39c8140be974e
kcaribou <kcaribou0428@gmail.com>
2021-02-17 23:28:24 +0800

    currencies table

---
+++ database/migrations/2021_02_17_145911_create_currencies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('currency', 100);
+            $table->integer('type')->default(1)->comment('1: local, 2:cripto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
