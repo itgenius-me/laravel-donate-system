be6b8af3776ece572eb3353b3d1ab210f6c87447
kcaribou <kcaribou0428@gmail.com>
2021-02-17 22:12:10 +0800

    change create orders table

--- database/migrations/2021_02_10_091429_create_create_orders_table.php
+++ database/migrations/2021_02_10_091429_create_create_orders_table.php
@@ -15,6 +15,19 @@ class CreateCreateOrdersTable extends Migration
     {
         Schema::create('create_orders', function (Blueprint $table) {
             $table->id();
+            $table->integer('gh_id')->default(0)->comment('id of get help');
+            $table->string('gh_email', 191)->comment('email user of get help');
+
+            $table->integer('ph_id')->default(0)->comment('id of provide help');
+            $table->string('ph_email', 191)->comment('email user of provide help');
+            $table->integer('ph_order_type')->default(0)->comment('0: 10% first, 1: 40% first, 2: 40% second, 3: 10% second');
+
+            $table->string('currency', 191)->comment('BRL, NGN, TRX, XRP, etc');
+            $table->double('match_order_amount')->comment('paid amount to get help');
+            $table->integer('status')->default(0)->comment('status of order(0: pending, 1:confirmed, 2: reject)');
+
+            $table->string('proof_attachment', 191)->comment('Attached Proof of Payment');
+            $table->dateTime('expired_date')->comment('the date that will be expired');
             $table->timestamps();
         });
     }
